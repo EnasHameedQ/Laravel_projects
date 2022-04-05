e9a58a2bbb89c6d147e918a4c2658fd2a0c3abb1
EnasHameed <alqaadienas@gmail.com>
2022-04-05 11:39:46 +0300

    insert data

---
+++ Enas_HR/resources/views/megration/megrate.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+  <!-- Required meta tags-->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+  <!-- Fontfaces CSS-->
+  <link href="assests/css/font-face.css" rel="stylesheet" media="all">
+
+  <!-- Bootstrap CSS-->
+  <link href="assests/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+
+  <link rel="stylesheet" type="text/css" href="assests/css/style.css" />
+  <link rel="stylesheet" type="text/css" href="assests/css/media-query.css" />
+  <link href="assests/css/theme.css" rel="stylesheet" media="all">
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS,  scroll    -->
+
+  <script src="assests/js/jquery-3.6.0.min.js"></script>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!--scroll  -->
+
+
+  <title>بروفايل 1</title>
+</head>
+
+<body>
+
+  <!-- start container nav -->
+  <div class="container">
+    <nav id="navbar" class="navbar  fixed-top navbar-expand-md navbar-light  navbar-right mt-0 ">
+      <!--  navbar-expand-md مهم لعرض مكونات المنيو في الشاشات الكبيرة والمتوسطة-->
+      <!--navbar-light مهن لعرض اقونة  البرقر في الشاشات الصغيرة -->
+      <!-- bg-light للخلفية الرمادي بدل الديفولت سوداء     / navbar-right اتجاه المنيو-->
+
+      <a class="navbar-brand" href="index.html">
+        <img class="img-responsive" src="assests/imges/human-resources.png ">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- burger icon only in mobil   -->
+
+      <div class="collapse navbar-collapse navbar-right" id="navbarSupportedContent">
+        <ul class="navbar-nav ">
+          <li class="nav-item ">
+            <a class="nav-link" href="index.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> الرئيسية
+            </a>
+          </li>
+          <!-- in <svg>  you can change color   fill="currentColor"    fill="#e63916"   currentColor mains baisk color of div -->
+          <li class="nav-item ">
+            <a class="nav-link" href="jobs.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> الوظائف
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="our_serveces.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i>خدماتنا
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link   active" href="our_parteners.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> شركاءنا
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="about_us.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> من نحن </a>
+          </li>
+
+
+          <li class="nav-item ">
+            <a class="nav-link" href="contuct_us.html"><i class='material-icons  float-right  &#xf0c8;' style='background-color: #FCC01A;'></i> تواصل معنا </a>
+          </li>
+
+          <li class="nav-item ">
+            <a class="nav-link" href="login.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> تسجيل الدخول
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="register.html"> <i class="material-icons float-right  &#xf0c8;" style='background-color: #FCC01A;'></i> انشاء حساب
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="assests/imges/boy.png " alt="mdo " width="32 " height="32 " class="rounded-circle ">
+            </a>
+            <ul class="dropdown-menu ">
+              <li><a class="dropdown-item " href="main-dashboard.html">الصفحة الشخصية</a></li>
+              <li>
+                <a class="dropdown-item " href="my_cv.html">سيرتي الذاتية</a>
+              </li>
+              <li>
+                <hr class="dropdown-divider ">
+              </li>
+              <li>
+                <!-- تبرمج لدعم لغتين -->
+                <a class=" dropdown-item " id="Language " ype="submit "> English</a>
+              </li>
+              <li><a class="dropdown-item " href="index.html">خروج</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+
+    </nav>
+  </div>
